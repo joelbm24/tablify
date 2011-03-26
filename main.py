316afebdeb154db0ce1e2325ffa10089f8e6cc04
Joel <joelbm24@gmail.com>
2011-03-25 21:56:47 -0700

    added better color suppport

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import Image, sys
 argv = sys.argv
 
 img = Image.open(argv[1])
-img = img.convert("RGB")
+img = img.convert("RGBA")
 
 pixdata = img.load()
 
@@ -12,6 +12,6 @@ f.write("<table cellspacing='0'>")
 for y in xrange(img.size[1]):
         f.write("<tr>")
         for x in xrange(img.size[0]):
-            f.write("<td width='0.9' height='0.9' style='background-color:rgb" + str(pixdata[x,y]) + "'></td>")
+            f.write("<td width='0.9' height='0.9' style='background-color:rgba" + str(pixdata[x,y]) + "'></td>")
         f.write("</tr>")
 f.write("</table>")
